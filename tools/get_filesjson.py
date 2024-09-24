36fd7a6c64dcef158734b62e849af2db02e9dae8
Ruirui-Huang <56825418+Ruirui-Huang@users.noreply.github.com>
2024-09-24 09:57:15 +0800

    <fix> update 93%

--- tools/get_filesjson.py
+++ tools/get_filesjson.py
@@ -11,28 +11,20 @@ def getArgs():
     parser.add_argument('filesInfo', type=str)
     return parser
 
-def generate_json(image_dir, json_dir, filesInfo):
-    # 如果JSON文件夹路径不存在，则创建文件夹
-    if not osp.exists(json_dir):
-        os.makedirs(json_dir, mode=0o777)
-
-    data = []
-
-    for image in os.listdir(image_dir):
-        if image.endswith('.jpg'):
-            image_path = osp.join(image_dir, image)
-            json_name = image.replace('.jpg', '.json')
-            json_path = osp.join(json_dir, json_name)
-            data.append({
-                "file": image_path,
-                "json": json_path
-            })
-
-    # 将数据写入新的JSON文件
-    with open(filesInfo, 'w') as outfile:
-        json.dump(data, outfile)
-
 if __name__ == '__main__': 
     parser = getArgs()
     args = parser.parse_args()
-    generate_json(args.image_dir, args.json_dir, args.filesInfo)
+    if not osp.exists(json_dir):
+        os.makedirs(json_dir, mode=0o777)
+    with open(args.filesInfo, 'w') as outfile:
+        for img in os.listdir(image_dir):
+            if img.endswith('.jpg'):
+                img_path = osp.join(args.image_dir, img)
+                json_path = osp.join(args.json_dir, img) + ".json"
+                data = {
+                    "baseId": "111",
+                    "file": image_path,
+                    "json": json_path
+                }
+                json.dump(data, outfile)
+                outfile.write('\n')
